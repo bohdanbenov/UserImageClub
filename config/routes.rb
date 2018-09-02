9dd66c37b10ad7f91f63dc6df7a2309e7b2a9b4d
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-02 22:23:52 +0300

    Edit routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'images/index'
   resources :users, only: [:index, :new, :create] do
-    resources :images, only: [:index]
+    resources :images, only: [:index, :destroy]
   end
   root 'users#index'
 end
