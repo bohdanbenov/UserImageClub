8182e2728b21c39fcc4f96cd7d0282169a0f500e
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-09 18:11:39 +0300

    Implemented basic uthentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'images/index'
   resources :users, only: [:index, :new, :create, :edit, :update, :destroy] do
     resources :images, only: [:index, :destroy, :update]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_185058) do
+ActiveRecord::Schema.define(version: 2018_09_09_145712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 2018_09_05_185058) do
     t.integer "age"
     t.string "sex"
     t.text "about"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
