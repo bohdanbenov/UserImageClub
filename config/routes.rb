af34d25ea8aa0bd8c08a18d2a6bc9bcbbaed8cb6
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-09 18:20:08 +0300

    Customized registration, login, and logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   get 'images/index'
   resources :users, only: [:index, :new, :create, :edit, :update, :destroy] do
     resources :images, only: [:index, :destroy, :update]
