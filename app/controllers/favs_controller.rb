9bde1ded05785488788b46d644631404c32b6963
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-12 11:26:58 +0300

    Delete before action.

--- app/controllers/favs_controller.rb
+++ app/controllers/favs_controller.rb
@@ -1,14 +1,6 @@
 class FavsController < ApplicationController
-  before_action :check_access, only: [:index]
   def index
     @user = User.find(params[:user_id])
     @all_urls = Fav.where(user_id: params[:user_id]).order(:id)
-  end
-
-  private
-  def check_access
-    unless @user == current_user || current_user.is_admin?
-      redirect_to root_path, :alert => "Access denied."
-    end
   end
 end
