c6bd3b5f91ba6be5cc47244089bfd33c5a4444a3
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-09-21 10:41:34 +0300

    Add destroy image test.

--- spec/requests/favourites_spec.rb
+++ spec/requests/favourites_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Favourites", type: :request do
-  let!(:image){create(:image, id: 12)}
+  # let!(:image){create(:image)}
   let!(:user){create(:user, password: '123456')}
 
   before do
@@ -20,8 +20,9 @@ RSpec.describe "Favourites", type: :request do
   end
 
   describe '#destroy' do
+    let!(:image){create(:image, user_id: user.id)}
     it "delete from images" do
-      expect{delete("/users/#{ user.id }/images/#{image.id}")}.to change{Fav.where(user_id: user.id, url: image.image_url).count}.from(1).to(0)
+      expect{delete("/users/#{ user.id }/images/#{image.id}")}.to change{Image.where(id: image.id).count}.from(1).to(0)
     end
   end
 
